9ab976d4bb4315aa854d29f28c239d838f6894a0
Snoilt <paul@oellers.net>
2023-03-30 16:01:21 +0200

    amogus

--- src/App.svelte
+++ src/App.svelte
@@ -1,52 +1,57 @@
 <script lang="ts">
 import * as THREE from 'three';
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader"
 
-//create scene and camera
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth/5, window.innerHeight/5, false)
+document.body.appendChild(renderer.domElement)
+renderer.domElement.style.height = "100%"
+renderer.domElement.style.width = "100%"
 
-//create renderer as big as window
-const renderer = new THREE.WebGLRenderer();
-//by adding .setSize( window.innerWidth, window.innerHeight, false) you change
-//the resolution its supposed to render add
-renderer.setSize( window.innerWidth/20, window.innerHeight/20, false);
-// add it as DOM Element
-document.body.appendChild( renderer.domElement );
-renderer.domElement.style = "width:100%;,height:100%;"
 
-//creating the cube 
-//it needs geometry, an object, which contains all the vertices and faces of the cube
-const geometry = new THREE.BoxGeometry( 2, 1, 1 );
-//a material which is just a plain  default material in the color green
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-//and a mesh. An Object that takes the geometry and applies the material to it accordingly
-const cube = new THREE.Mesh( geometry, material );
 
-//add the cube to the scene
-scene.add( cube );
+const geometry = new THREE.ConeGeometry(1.5,1.5,3)
+const material = new THREE.MeshBasicMaterial()
+const cube = new THREE.Mesh(geometry,material)
 
-//move the camera out as the cube and the camera would be located inside each other 
-camera.position.z = 5;
 
-//this will render the screen everytime the screen is refreshed
+camera.position.z = 5
+
+const backGroundColor = new THREE.Color(0x000000)
+
+
+const amongium = new THREE.TextureLoader().load("/src/assets/amongus.jpeg")
+amongium.wrapS = THREE.RepeatWrapping
+amongium.wrapT = THREE.RepeatWrapping
+amongium.repeat.set(3,1)
+amongium.rotation = -0.155
+material.map = amongium
+
+scene.background = backGroundColor
+
+
+
+
+scene.add(cube)
+
 const animate = () => {
     requestAnimationFrame( animate )
-    cube.rotation.x += 0.01
     cube.rotation.y += 0.01
     renderer.render(scene, camera)
 }
 
-
-
-
-
 animate()
+
+
+
 
 </script>
 
 <main>
-    <div id="info">Cube?????</div>
+    <div id="info">Sus????????ß</div>
 </main>
 
 <style lang="scss">
@@ -57,8 +62,8 @@ animate()
 	text-align: center;
 	z-index: 100;
 	display:block;
-    color:white;
     font-size: 50px;
     padding-top:200px;
+    color: white;
 }
 </style>
